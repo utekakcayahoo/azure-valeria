8067cf0ed592b95b8414569f2fac66a37f5d4be1
Umut Tekakca <utekakca@gmail.com>
2024-12-26 12:34:23 +0300

    amend exampleu

--- main.tf
+++ main.tf
@@ -16,57 +16,57 @@ provider "azurerm" {
 
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "exampleu" {
+  name     = "exampleu-resources"
   location = "West Europe"
 }
 
-resource "azurerm_data_share_account" "example" {
-  name                = "example-dsa"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_data_share_account" "exampleu" {
+  name                = "exampleu-dsa"
+  location            = azurerm_resource_group.exampleu.location
+  resource_group_name = azurerm_resource_group.exampleu.name
   identity {
     type = "SystemAssigned"
   }
 }
 
-resource "azurerm_data_share" "example" {
-  name       = "example_ds"
-  account_id = azurerm_data_share_account.example.id
+resource "azurerm_data_share" "exampleu" {
+  name       = "exampleu_ds"
+  account_id = azurerm_data_share_account.exampleu.id
   kind       = "CopyBased"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestr"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "exampleu" {
+  name                     = "exampleustr"
+  resource_group_name      = azurerm_resource_group.exampleu.name
+  location                 = azurerm_resource_group.exampleu.location
   account_kind             = "BlobStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
-  name               = "example-dlg2fs"
-  storage_account_id = azurerm_storage_account.example.id
+resource "azurerm_storage_data_lake_gen2_filesystem" "exampleu" {
+  name               = "exampleu-dlg2fs"
+  storage_account_id = azurerm_storage_account.exampleu.id
 }
 
-data "azuread_service_principal" "example" {
-  display_name = azurerm_data_share_account.example.name
+data "azuread_service_principal" "exampleu" {
+  display_name = azurerm_data_share_account.exampleu.name
 }
 
-resource "azurerm_role_assignment" "example" {
-  scope                = azurerm_storage_account.example.id
+resource "azurerm_role_assignment" "exampleu" {
+  scope                = azurerm_storage_account.exampleu.id
   role_definition_name = "Storage Blob Data Reader"
-  principal_id         = data.azuread_service_principal.example.object_id
+  principal_id         = data.azuread_service_principal.exampleu.object_id
 }
 
-resource "azurerm_data_share_dataset_data_lake_gen2" "example" {
-  name               = "accexample-dlg2ds"
-  share_id           = azurerm_data_share.example.id
-  storage_account_id = azurerm_storage_account.example.id
-  file_system_name   = azurerm_storage_data_lake_gen2_filesystem.example.name
+resource "azurerm_data_share_dataset_data_lake_gen2" "exampleu" {
+  name               = "accexampleu-dlg2ds"
+  share_id           = azurerm_data_share.exampleu.id
+  storage_account_id = azurerm_storage_account.exampleu.id
+  file_system_name   = azurerm_storage_data_lake_gen2_filesystem.exampleu.name
   file_path          = "myfile.txt"
   depends_on = [
-    azurerm_role_assignment.example,
+    azurerm_role_assignment.exampleu,
   ]
 }
